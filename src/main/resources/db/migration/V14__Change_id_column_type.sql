da2e600b6c5feab2f28c0f5fb90adc2bfcf68f1a
GuoFH <891448235@qq.com>
2020-03-30 02:59:20 +0800

    修复数据库

--- src/main/resources/db/migration/V14__Change_id_column_type.sql
+++ src/main/resources/db/migration/V14__Change_id_column_type.sql
@@ -1,2 +1,2 @@
-alter table POST alter column ID auto_increment  not null ;
-alter table USER alter column ID auto_increment not null ;
+alter table POST alter column ID bigint auto_increment  not null ;
+alter table USER alter column ID bigint auto_increment not null ;
